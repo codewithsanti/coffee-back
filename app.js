8b1fbdfce4c3dbc8d4c3866cac7815a5a117f449
santiagobenfatto <santiagobenfatto@outlook.com>
2024-02-05 17:46:39 -0300

    Initialized server on express

---
+++ app.js
@@ -0,0 +1,12 @@
+import express from 'express'
+
+const app = express()
+const PORT = process.env.PORT || 3001 
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+
+app.listen(PORT, ()=> {
+    console.log(`Server running and listening on port ${PORT}`)
+})
